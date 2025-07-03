d92ed822a52a24062ff55054f428b4d1e134ca1b
Vasily Seleznev <v.seleznev@geoscan.ru>
2025-07-03 15:29:10 +0300

    Исправление импорта BaseORM

--- src/infrastructure/postgres/user/models.py
+++ src/infrastructure/postgres/user/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from infrastructure.postgres.base_model import BaseORM
+from infrastructure.postgres.base import BaseORM
 from infrastructure.postgres.mixins import TimestampMixin, UUIDMixin
 
 
