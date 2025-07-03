fa404e1ee18a131e0608977479d0d40278438d96
Vasily Seleznev <v.seleznev@geoscan.ru>
2025-07-03 13:58:24 +0300

    Удалена автогенерация имени ORM модели

--- src/infrastructure/postgres/user/models.py
+++ src/infrastructure/postgres/user/models.py
@@ -5,7 +5,9 @@ from infrastructure.postgres.base_model import Base
 from infrastructure.postgres.mixins import TimestampMixin, UUIDMixin
 
 
-class User(Base, UUIDMixin, TimestampMixin):
+class UserORM(Base, UUIDMixin, TimestampMixin):
+    __tablename__ = "users"
+
     username: Mapped[str] = mapped_column(String(128), unique=True)
     email: Mapped[str] = mapped_column(String(128), unique=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
