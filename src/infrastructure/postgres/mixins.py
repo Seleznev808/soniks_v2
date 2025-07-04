c80fb87eb34d2c955b32158f41ce35feca77dc14
Vasily Seleznev <v.seleznev@geoscan.ru>
2025-07-04 16:32:52 +0300

    Добавлена таймзона в TimestampMixin (UTC по-умолчанию)

--- src/infrastructure/postgres/mixins.py
+++ src/infrastructure/postgres/mixins.py
@@ -2,16 +2,19 @@ from datetime import datetime
 from uuid import UUID, uuid4
 
 from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
         default=func.now(),
         server_default=func.now(),
     )
     updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
         default=func.now(),
         onupdate=func.now(),
         server_default=func.now(),
